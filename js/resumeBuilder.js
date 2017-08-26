15634f7ea4cd5a7134b9be621b851f13364faa67
Rahul Kathuria <rahulkathuria52@gmail.com>
2017-08-26 22:06:02 +0530

    updated objects

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,54 @@
-/*
-This is empty on purpose! Your code to build the resume will go here.
- */
-$("#main").append(["Rahul"]); 
+var bio = {
+      "name" : "Rahul Kathuria",
+      "role" : "Android Developer",
+      "contacts" : { 
+            "mobile": "+918684868265",
+            "email": "rahulkathuria52@gmail.com",
+            "github": "www.github.com/RahulKathuria",
+            "twitter": "@rahulkathuria52",
+            "location": "Bhiwani"
+      },
+      "welcomeMessage": "Hello Visitor ",
+      "skills": ["Java","C++","Android","Web-Development"],
+      "biopic": "images/biopic.JPG"
+
+};
+var education = {
+        schools: [{
+           "name": "Bhiwani Public School",
+           "location": "Bhiwani",
+           "degree": "Metric",
+           "majors": ["Science","Math","English","Computer Science"],
+           "dates": "1-march-2015",
+           "url": "www.bpsbhiwani.org"}],
+      onlineCourses: [{
+           "title": "Android Developer Nanodegree",
+           "school": "Udacity",
+           "dates": "1-January-2017",
+           "url": "www.udacity.com"}]
+};
+var work = {
+        jobs: [{
+           "employer": "Udacity",
+           "title": "mentor",
+           "location": "India",
+           "dates": "1-August-2017",
+           "description": "Mentor at Udacity" 
+           ]}
+      
+};
+var projects = {
+       projects:[{
+            "title": "Portfolio",
+            "dates": "1-march-2017",
+            "description": "Project of Front End Nanodegree",
+            "images": ["images/project1.png","images/project1_1.png"]},
+            {"title": "Reverse-mad-libs", 
+            "dates": "1-march-2017",
+            "description": "Project of IPND",
+            "images": ["images/project2.png"]}
+            ]
+};
+bio.display=function(){
+      HTML
+}
